519386d73d41206fa2e3b8522c489205dba0959d
nguyentienhuy123 <110875263+nguyentienhuy123@users.noreply.github.com>
2022-11-17 10:02:11 +0700

    Add project files.

---
+++ OnlineShop_HuyNT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineShop_HuyNT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
